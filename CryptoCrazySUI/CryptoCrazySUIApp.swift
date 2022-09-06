ee07f80c62d04a9a07837e1484562d58c246295e
alperenkavukk <kavuk.67.76@gmail.com>
2022-09-06 14:47:56 +0300

    Initial Commit

---
+++ CryptoCrazySUI/CryptoCrazySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySUIApp.swift
+//  CryptoCrazySUI
+//
+//  Created by Alperen Kavuk on 6.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
